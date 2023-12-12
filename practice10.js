3c6f7759f6345d3da4893328e800e4ff96235d1f
Hanna Barna <hanna.barna@teladochealth.com>
2023-12-12 15:49:42 +0200

    Update practice10.js

--- practice10.js
+++ practice10.js
@@ -4,25 +4,35 @@ const os = require('node:os')
 const path = require('path')
 
 // Task 1
+const currentUser = os.userInfo().username
+const osType = os.version()
+const osUptime = os.uptime()
+const currentDir = __dirname
+const currentFile = __filename
+const serverFileName = path.basename(currentFile)
+
 http.createServer(function (request, response) {
   response.writeHead(200, { 'Content-Type': 'text/html' })
-  response.write('<H2>System information</H2>')
-  response.write('<p>Current username: ' + os.userInfo().username)
-  response.write('<p>OS type: ' + os.version())
-  response.write('<p>System worktime: ' + os.uptime())
-  response.write('<p>Current work directory: ' + __dirname)
-  response.write('<p>Server file name: ' + path.basename(__filename))
-  response.end('<p>Testing NodeJS server\n')
+  response.write(`<H2>System information</H2>
+                  <p>Current username: ${currentUser}
+                  <p>OS type: ${osType}
+                  <p>System worktime: ${osUptime}
+                  <p>Current work directory: ${currentDir}
+                  <p>Server file name: ${serverFileName}`)
+  response.end('<p>')
 }).listen(5000)
 
 console.log('Server running at http://127.0.0.1:5000/')
 
 // Task 2
+const currentDate = new Date()
+const userGreeting = myModule.userGreeting(currentUser)
+
 http.createServer(function (request, response) {
   response.writeHead(200, { 'Content-Type': 'text/html' })
-  response.write('<p>Day of the request: ' + new Date())
-  response.write('<p>' + myModule.userGreeting(os.userInfo().username))
-  response.end('<p>Testing NodeJS server\n')
+  response.write(`<p>Day of the request: ${currentDate}
+                  <p> ${userGreeting}`)
+  response.end('<p>')
 }).listen(8000)
 
 console.log('Server running at http://127.0.0.1:8000/')
