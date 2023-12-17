fe6854f502c614db43fef1265159cf36ec942465
Hanna Barna <hanna.barna@teladochealth.com>
2023-12-17 23:23:46 +0200

    Add practice11

--- practice11/task1/server.js
+++ practice11/task1/server.js
@@ -1,6 +1,18 @@
 const http = require('http')
 const fs = require('fs')
 
+const urls = {
+  homePage: '/',
+  updateButton: '/app.js',
+  getDate: '/date'
+}
+
+const resourses = new Map()
+resourses.set(urls.homePage, 'index.html')
+resourses.set(urls.updateButton, 'app.js')
+const date = new Date()
+resourses.set(urls.getDate, date.toString())
+
 http.createServer((request, response) => {
   function buildResponse (content) {
     response.writeHead(200, { 'Content-Type': 'text/html' })
@@ -13,23 +25,22 @@ http.createServer((request, response) => {
     response.end('404 Not Found')
   }
 
-  if (request.url === '/') {
-    fs.readFile('index.html', (err, content) => {
+  if (request.url === urls.homePage) {
+    fs.readFile(resourses.get(request.url), (err, content) => {
       if (err) showErrorScreen()
       else buildResponse(content)
     })
   }
 
-  if (request.url === '/app.js') {
-    fs.readFile('app.js', (err, content) => {
+  if (request.url === urls.updateButton) {
+    fs.readFile(resourses.get(request.url), (err, content) => {
       if (err) showErrorScreen()
       else buildResponse(content)
     })
   }
 
-  if (request.url === '/date') {
-    const date = new Date()
-    buildResponse(date.toString())
+  if (request.url === urls.getDate) {
+    buildResponse(resourses.get(request.url))
   }
 }).listen(3000)
 
--- practice11/task2/server.js
+++ practice11/task2/server.js
@@ -1,6 +1,18 @@
 const http = require('http')
 const fs = require('fs')
 
+const urls = {
+  homePage: '/',
+  replaceButton: '/app.js',
+  download: '/download'
+}
+
+const resourses = new Map()
+resourses.set(urls.homePage, 'index.html')
+resourses.set(urls.replaceButton, 'app.js')
+const list = ['David Flanagan', 'Kylie Moor', 'John Lee', 'Jeremy Doe']
+resourses.set(urls.download, JSON.stringify(list))
+
 http.createServer((request, response) => {
   function buildResponse (content) {
     response.writeHead(200, { 'Content-Type': 'text/html' })
@@ -13,23 +25,22 @@ http.createServer((request, response) => {
     response.end('404 Not Found')
   }
 
-  if (request.url === '/') {
-    fs.readFile('index.html', (err, content) => {
+  if (request.url === urls.homePage) {
+    fs.readFile(resourses.get(request.url), (err, content) => {
       if (err) showErrorScreen()
       else buildResponse(content)
     })
   }
 
-  if (request.url === '/app.js') {
-    fs.readFile('app.js', (err, content) => {
+  if (request.url === urls.replaceButton) {
+    fs.readFile(resourses.get(request.url), (err, content) => {
       if (err) showErrorScreen()
       else buildResponse(content)
     })
   }
 
-  if (request.url === '/download') {
-    const list = ['David Flanagan', 'Kylie Moor', 'John Lee', 'Jeremy Doe']
-    buildResponse(JSON.stringify(list))
+  if (request.url === urls.download) {
+    buildResponse(resourses.get(request.url))
   }
 }).listen(3000)
 
